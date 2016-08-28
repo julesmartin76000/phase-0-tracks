86b7452151a8306f8c4362897f14bf0d953db9fb
Jules Martin <julesmartin76000@gmail.com>
2016-08-28 10:59:20 -0700

    Assignment 4.4 end

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,7 +4,7 @@ employee = gets.chomp.to_i
 i=1
 while i<=employee 
 
-puts "what is your name ?"
+	puts "what is your name ?"
 	name =gets.chomp
 	puts "what is the current year ?"
 	year =gets.chomp.to_i
@@ -19,39 +19,13 @@ puts "what is your name ?"
 
 
 
-if name =="Drake Cula"
-	true_name= false 
-elsif name == "Tu Fang"
-	true_name= false
-else true_name= true
-end 
-
-if age == year - born
-	wolves_live_less=true
-else
-	wolves_live_less=false
-end
-
-if garlic_bread == "y"
-	wolves_like_garlic = true
-else
-	wolves_like_garlic = false
-end
-
-if insurance == "y"
-	wolves_need_insurance = true
-else
-	wolves_need_insurance = false
-end
-
-if wolves_live_less && wolves_like_garlic && wolves_need_insurance
-	puts "Probably not a vampire."
-elsif wolves_like_garlic || wolves_need_insurance
-	puts "Probably a vampire."
-elsif 
-	puts "Almost certainly a vampire."
-end 
-
+	if name =="Drake Cula"
+		true_name= false 
+	elsif name == "Tu Fang"
+		true_name= false
+	else true_name= true
+	end 
+	
 	if age == year - born
 		wolves_live_less=true
 	else
@@ -70,13 +44,30 @@ end
 		wolves_need_insurance = false
 	end
 	
+	sunshine = FALSE
+
+	until sunshine
+		puts "Do you have any allergie ? (one at a time, type done when finished)"
+		
+		allergies = gets.chomp
+		if allergies == "done"
+		sunshine = true
+		puts "We have recorded your informations"
+		elsif allergies == "sunshine"
+		sunshine = true 
+		else 
+		end
+	end 
+
 	if wolves_live_less && wolves_like_garlic && wolves_need_insurance
 		puts "Probably not a vampire."
-	elsif wolves_like_garlic || wolves_need_insurance
+	elsif wolves_like_garlic || wolves_need_insurance || sunshine
 		puts "Probably a vampire."
 	elsif 
 		puts "Almost certainly a vampire."
 	end 
 i+=1 
 end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 
