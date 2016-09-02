407775fec2baca8439fa5cf4fd9d6add29fcbbcf
Jules Martin <julesmartin76000@gmail.com>
2016-09-01 17:25:52 -0700

    working on 5.4

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,21 +1,21 @@
 classroom = {
 	desk_1:{
 		english_book:'How to learn english',
-		pencil_case:{
+		pencil_case:[
 			"red pen": 2,
 			"green pen": 3
-		}
+		]
 	},
 	desk_2:{
 		french_book:'How to learn french',
 		diary: '300 hundreds pages'
-	}
+	},
 	desk_3:{
-		spanish_book:'How to learn spanish'
-		pencil_case:{
+		spanish_book:'How to learn spanish',
+		pencil_case:[
 			"red pen": 4,
 			"green pen": 5,
-		}
+		]
 	}
 }
 
