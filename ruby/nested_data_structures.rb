a53ac40caa35aa8b580b555c67156cb8ac0b05ba
Jules Martin <julesmartin76000@gmail.com>
2016-09-05 14:00:24 -0700

    end of 5.4

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,10 +1,10 @@
 classroom = {
 	desk_1:{
 		english_book:'How to learn english',
-		pencil_case:[
+		pencil_case:{
 			"red pen": 2,
 			"green pen": 3
-		]
+		}
 	},
 	desk_2:{
 		french_book:'How to learn french',
@@ -13,11 +13,12 @@ classroom = {
 	desk_3:{
 		spanish_book:'How to learn spanish',
 		pencil_case:[
-			"red pen": 4,
-			"green pen": 5,
+			"red pen: 4",
+			"green pen: 5",
 		]
 	}
 }
 
 #add a bllue pen in desk_3
-classroom[:desk_3][:pencil_case].push("blue pen": 1)
+classroom[:desk_3][:pencil_case].push("blue pen: 1")
+classroom[:desk_3][:pencil_case].reverse 
