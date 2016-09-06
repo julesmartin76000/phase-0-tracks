231302522b4a77eb5f9b80db1c334d6f4173f1a3
Jules Martin <julesmartin76000@gmail.com>
2016-09-06 16:27:32 -0700

    good file now

---
+++ ruby/puppy.rb
@@ -0,0 +1,11 @@
+#CHARACTERISTICS ==> Attributes
+#Eyelash length: varies
+#Friendly: yes
+#Eye count: 2
+#Name: varies
+#Color: many
+
+#BEHAVIOR ==> Methods
+#fetch 
+#roll over
+#offer a high five
