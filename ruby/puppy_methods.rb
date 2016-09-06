eca0824867ba009c17c4bf97ee9f70bb3bc10ead
Jules Martin <julesmartin76000@gmail.com>
2016-09-06 16:13:05 -0700

    end of 6.1

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,19 @@
+#SPECIES ==> Classes
+#Puppies
+
+#CHARACTERISTICS ==> Attributes
+#Eyelash length: varies
+#Friendly: yes
+#Eye count: 2
+#Name: varies
+#Color: many
+
+#BEHAVIOR ==> Methods
+#fetch 
+#roll over
+#offer a high five
+
+
 class Puppy
 
   def fetch(toy)
