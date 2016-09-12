732e22cf337a41d2b45461580ac2134f54566bb7
Jules Martin <julesmartin76000@gmail.com>
2016-09-12 10:35:13 -0700

    correction of 4.3

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,21 +1,21 @@
 puts "What is the hamster name ?"
 name = gets.chomp
-puts "
-On a scale from 1-10, what is the hamsters noise volume? (please enter a number)"
+puts "On a scale from 1-10, what is the hamsters noise volume? (please enter a number)"
 volume = gets.chomp.to_i
 puts "What is the fur color ?"
-fur = gets.chomp
+flur = gets.chomp
 puts "Is the hamster a good candidate for adoption? (yes/no)"
 candidate = gets.chomp
 puts "Estimated age of hamster"
 age= gets.chomp.to_i
-if age == "".to_i
+if age == ""
 	then age = nil
+	age == "unknown"
 end
 
-puts "Hamster's data"
+puts "Hamster's data :"
 puts "Hamster's name is #{name}"
 puts "Hamster's volume is #{volume}"
-puts "Flur color is #{colour}"
-puts "Is the hamster a good candidate for adoption? (yes/no) "
-puts "Hamster's age is #{}"
+puts "Flur color is #{flur}"
+puts "Is the hamster a good candidate for adoption? : #{candidate} "
+puts "Hamster's age is #{age}"
