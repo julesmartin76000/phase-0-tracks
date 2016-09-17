eaa5d5f02513438246eb37fe0bcd2a5f81dc9820
Jules Martin <julesmartin76000@gmail.com>
2016-09-17 15:42:41 -0700

    release 1 done

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,7 +1,14 @@
 var colours = ["Blue", "Red", "Yellow", "Green"];
 var names = ["Buster", "Princess Fluffypaws", "Minxy", "Ed"];
+var ranch = {}
 
 colours.push("purple");
 names.push("Freddy"); 
 
-console.log(names, colours);
+// console.log(names, colours);
+
+for (var i = 0; i < names.length; i++) {
+	ranch[names[i]] = colours[i]; 
+}
+
+console.log(ranch)
