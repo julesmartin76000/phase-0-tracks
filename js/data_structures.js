f8489f602e927557fcc1075dfb88ebe871852de9
Jules Martin <julesmartin76000@gmail.com>
2016-09-17 15:14:23 -0700

    release 0 done

---
+++ js/data_structures.js
@@ -0,0 +1,7 @@
+var colours = ["Blue", "Red", "Yellow", "Green"];
+var names = ["Buster", "Princess Fluffypaws", "Minxy", "Ed"];
+
+colours.push("purple");
+names.push("Freddy"); 
+
+console.log(names, colours);
