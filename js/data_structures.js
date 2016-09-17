05451208c38215a4036c438d024ad0749cc7fb9d
Jules Martin <julesmartin76000@gmail.com>
2016-09-17 16:11:48 -0700

    release 2 done

--- js/data_structures.js
+++ js/data_structures.js
@@ -12,3 +12,26 @@ for (var i = 0; i < names.length; i++) {
 }
 
 console.log(ranch)
+
+// Release 2
+
+function Car(color, seats, options) {
+	this.color = color;
+	this.seats = seats;
+	this.options = options; 
+	
+	this.honk = function() { console.log("Beep Beep"); };
+}
+
+
+console.log("Let's build a car ...");
+
+var newCar = new Car("red", 4, true);
+console.log(newCar);
+console.log("Our can beep:");
+newCar.honk();
+console.log("----");
+
+var anotherCar = new Car("blue", 5, false);
+console.log(anotherCar);
+anotherCar.honk();
