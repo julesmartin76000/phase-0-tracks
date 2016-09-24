28792c539bc1acb6d6493af57316127a52524690
Jules Martin <julesmartin76000@gmail.com>
2016-09-23 18:09:24 -0700

    working, have to add specificities

--- databases/8.5_project/project.rb
+++ databases/8.5_project/project.rb
@@ -24,15 +24,22 @@ db.execute(create_table_cmd)
 #db.execute("INSERT INTO running (city_name, stars) VALUES ('San Francisco', 5)")
 
 def store_city(db, city_name, stars)
-	db.execute("INSERT INTO running (city_name, stars) VALUES ('San Francisco', 5)")
+	db.execute("INSERT INTO running (city_name, stars) VALUES (?, ?)", [city_name, stars])
+	#db.execute("SELECT * FROM running")
 end 
 
+10.times do 
+	store_city(db, Faker::Address.city, 3)
+end 
 
 # explore ORM by retrieving data
 #running = db.execute("SELECT * FROM running")
 #running.each do |run|
 #	puts "#{run['city_name']} has #{run['stars']} stars."
 #end 
+
+###### The purpose is to ask for the datas to an user and save 
+###### the number of stars to create a ranking
 
 
 
