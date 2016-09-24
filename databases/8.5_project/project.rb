0ce588d9a6fe16f001ba5ccc2fa8204b57e0468b
Jules Martin <julesmartin76000@gmail.com>
2016-09-23 17:56:33 -0700

    working on the project

--- databases/8.5_project/project.rb
+++ databases/8.5_project/project.rb
@@ -4,12 +4,13 @@ require 'faker'
 
 # create SQLite3 database : city where it's cool to run 
 db = SQLite3::Database.new("running.db")
+db.results_as_hash = true 
 		#db.execute("SELECT * FROM running")
 
 # Learn about fancy string delimiters ==> Don't forget, this just a string, 
 # I am not running SQL in my database.
 create_table_cmd = <<-SQL
-CREATE TABLE running(
+CREATE TABLE IF NOT EXISTS running(
 	id INTEGER PRIMARY KEY,
 	city_name VARCHAR(255),
 	stars INT
@@ -18,5 +19,23 @@ SQL
 
 #create a running table 
 db.execute(create_table_cmd)
+
+# add a test running
+#db.execute("INSERT INTO running (city_name, stars) VALUES ('San Francisco', 5)")
+
+def store_city(db, city_name, stars)
+	db.execute("INSERT INTO running (city_name, stars) VALUES ('San Francisco', 5)")
+end 
+
+
+# explore ORM by retrieving data
+#running = db.execute("SELECT * FROM running")
+#running.each do |run|
+#	puts "#{run['city_name']} has #{run['stars']} stars."
+#end 
+
+
+
+
 
 
